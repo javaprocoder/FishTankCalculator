dcce235045552557f1b92df1aa4d84a8e1adf415
saadrushdi246 <saadrushdi246@gmail.com>
2025-02-21 05:00:37 +0000

    for loop usage.

--- loops.py
+++ loops.py
@@ -1,5 +1,11 @@
+#-------------------------DAY 02 PYTHON WORKSHOP-------------------------
+ 
 #While LOOPS
 count = 0
 while count < 6:
     print("I Love Sri-Lanka")
     count +=1
+
+#For LOOPS
+for count in range(3):
+    print("I Love Python")
