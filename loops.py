bf0ddd30d369f8798fdbd9706b66f4d08c154658
saadrushdi246 <saadrushdi246@gmail.com>
2025-02-21 05:27:49 +0000

    Lists usage

--- loops.py
+++ loops.py
@@ -7,5 +7,12 @@ while count < 6:
     count +=1
 
 #For LOOPS
-for count in range(3):
+for count in range(1,6):
+    print(count)
     print("I Love Python")
+
+#LISTS
+grocery_list = ["Flour", "Rice", "Sugar", "Dhal"]
+
+for item in grocery_list:
+    print("Buying", item)
