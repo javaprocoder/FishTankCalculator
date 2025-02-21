a78b3203c1042a9dcf74feaf55de9fc4a41cddba
saadrushdi246 <saadrushdi246@gmail.com>
2025-02-21 09:48:49 +0000

    Objects in Python

--- loops.py
+++ loops.py
@@ -12,22 +12,22 @@ for count in range(1,6):
     print("I Love Python")
 
 #LISTS
-grocery_list = ["Flour", "Rice", "Sugar", "Dhal"]
-total_cost = 0
+# grocery_list = ["Flour", "Rice", "Sugar", "Dhal"]
+# total_cost = 0
 
-for item in grocery_list:
-    if (item == "Sugar"):
-        continue
-    print("Buying", item)
+# for item in grocery_list:
+#     if (item == "Sugar"):
+#         continue
+#     print("Buying", item)
 
-    item_price = int( input("Enter Current Item Price :"))
-    total_cost += item_price
-    print("Current Total Cost :", total_cost)
+#     item_price = int( input("Enter Current Item Price :"))
+#     total_cost += item_price
+#     print("Current Total Cost :", total_cost)
 
-    if total_cost > 500:
-        break
+#     if total_cost > 500:
+#         break
 
-print("Have a Happy Shopping!")
+# print("Have a Happy Shopping!")
     
 
 #Taking USER INPUT
@@ -40,3 +40,21 @@ print("Have a Happy Shopping!")
 # else: 
 #     print("You are not Eligible to Enroll")
 
+#Activity 01
+moms_grocery_list = ["Sugar", "Bread", "Pasta", "Noodles"]
+max_price_list = [350,490,820,680]
+total_cost_moms = 0
+
+for item in moms_grocery_list:
+    if (item == "Sugar"):
+        continue
+    print("Buying", item)
+
+    item_price = int( input("Enter Current Item Price :"))
+    total_cost_moms += item_price
+    print("Current Total Cost :", total_cost_moms)
+
+    if total_cost_moms > 600:
+        break
+
+print("You Have Done a Great Shopping My Son")
---
+++ objets.py
@@ -0,0 +1,15 @@
+#-------------------------DAY 02 PYTHON WORKSHOP PART 2-------------------------
+#FUNCTIONS
+def morning_greetin_message():
+    print("Hello")
+    print("Good Morning")
+    print("How are you ?")
+
+#Meeting the first person
+morning_greetin_message()
+
+#Meeting the second person
+morning_greetin_message()
+
+#Meeting the third person
+morning_greetin_message()
