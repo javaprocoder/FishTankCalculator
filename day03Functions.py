030f9c55284037a66d297439dd8d1f321ef9b000
saadrushdi246 <saadrushdi246@gmail.com>
2025-02-28 05:24:17 +0000

    Add a switch

--- day03Functions.py
+++ day03Functions.py
@@ -1,36 +1,38 @@
-#-------------------------DAY 03 PYTHON WORKSHOP-------------------------
-#------------------------CALCULATOR--------------------
-#FUNCTIONS AS ARGUMENTS
-def add(num1,num2):
-    return num1+num2
+# -------------------------DAY 03 PYTHON WORKSHOP-------------------------
+# ------------------------CALCULATOR--------------------
+# FUNCTIONS AS ARGUMENTS
+def add(num1, num2):
+    return num1 + num2
 
-def multiply(num1,num2):
-    return(num1*num2)
+def multiply(num1, num2):
+    return num1 * num2
 
-def substract(num1,num2):
-    return(num1-num2)
+def substract(num1, num2):  
+    return num1 - num2
 
-def divide(num1,num2):
-    return(num1/num2)
+def divide(num1, num2):
+    if num2 == 0:
+        return "Error! Division by zero."
+    return num1 / num2
 
-# print(add(10,30))
-# print(add(8,5))
+while True:  
+    num1 = int(input("Enter Number One: "))
+    num2 = int(input("Enter Number Two: "))
 
-# #print(add(2,5) - (substract(10-2)) * 2 /2)
-# print(substract(add(2,5),multiply(10,2)/2))
+    math_operation = input("Enter The Operator Symbol (+, -, *, /): ")
 
-num1 = int(input("Enter Number One:"))
-num2 = int(input("Enter Number two:"))
+    if math_operation == "+":
+        print("Result:", add(num1, num2))
+    elif math_operation == "-":
+        print("Result:", substract(num1, num2))
+    elif math_operation == "*":
+        print("Result:", multiply(num1, num2))
+    elif math_operation == "/":
+        print("Result:", divide(num1, num2))
+    else:
+        print("Invalid Operation!")
 
-math_operation = input("Enter The Operator Symbol:")
-
-if math_operation == "+":
-    print(add(num1,num2))
-elif math_operation == "-":
-    print(substract(num1,num2))
-elif math_operation == "*":
-    print(multiply(num1,num2))
-elif math_operation == "/":
-    print(divide(num1,num2))
-else:
-    print("Invalid Operation!")
+    repeat = input("Do you want to perform another calculation? (y/n): ").strip().lower()
+    if repeat != "y":
+        print("Goodbye!")
+        break  
