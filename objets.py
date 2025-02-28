4332b20d48e7d7ecee479500d4a5a619394b9085
saadrushdi246 <saadrushdi246@gmail.com>
2025-02-28 06:44:49 +0000

    Nested Loops

--- objets.py
+++ objets.py
@@ -16,3 +16,16 @@ morning_greetin_message("Saad")
 
 #Meeting the third person
 morning_greetin_message("Sharaf")
+
+#Nested Loops -> Loops inside another Loop
+a = 1
+while (a<5):
+    print(a)
+
+    b = 1
+    while(b<5):
+        print(b)
+        b+=1
+
+    a+=1
+    
