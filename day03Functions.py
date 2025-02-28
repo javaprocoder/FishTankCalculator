aa685d42f415a60ec311804f4935860c6ebb867b
saadrushdi246 <saadrushdi246@gmail.com>
2025-02-28 05:10:40 +0000

    Created Basic Calculator

--- day03Functions.py
+++ day03Functions.py
@@ -2,7 +2,7 @@
 #------------------------CALCULATOR--------------------
 #FUNCTIONS AS ARGUMENTS
 def add(num1,num2):
-    return(num1+num2)
+    return num1+num2
 
 def multiply(num1,num2):
     return(num1*num2)
@@ -18,3 +18,19 @@ def divide(num1,num2):
 
 # #print(add(2,5) - (substract(10-2)) * 2 /2)
 # print(substract(add(2,5),multiply(10,2)/2))
+
+num1 = int(input("Enter Number One:"))
+num2 = int(input("Enter Number two:"))
+
+math_operation = input("Enter The Operator Symbol:")
+
+if math_operation == "+":
+    print(add(num1,num2))
+elif math_operation == "-":
+    print(substract(num1,num2))
+elif math_operation == "*":
+    print(multiply(num1,num2))
+elif math_operation == "/":
+    print(divide(num1,num2))
+else:
+    print("Invalid Operation!")
