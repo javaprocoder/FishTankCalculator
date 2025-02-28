5e6c2ee1450a2c2cc996f8b375bac4889e265a7e
saadrushdi246 <saadrushdi246@gmail.com>
2025-02-28 05:01:25 +0000

    Creating calulation functions

---
+++ day03Functions.py
@@ -0,0 +1,20 @@
+#-------------------------DAY 03 PYTHON WORKSHOP-------------------------
+#------------------------CALCULATOR--------------------
+#FUNCTIONS AS ARGUMENTS
+def add(num1,num2):
+    return(num1+num2)
+
+def multiply(num1,num2):
+    return(num1*num2)
+
+def substract(num1,num2):
+    return(num1-num2)
+
+def divide(num1,num2):
+    return(num1/num2)
+
+# print(add(10,30))
+# print(add(8,5))
+
+# #print(add(2,5) - (substract(10-2)) * 2 /2)
+# print(substract(add(2,5),multiply(10,2)/2))
