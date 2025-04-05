d73e5c414bd6e6956ab521b2a497b11e38565692
saadrushdi246 <saadrushdi246@gmail.com>
2025-04-05 12:04:28 +0000

    for loop.

--- EEI3372 PYTHON Intro Level 1/day2.py
+++ EEI3372 PYTHON Intro Level 1/day2.py
@@ -10,3 +10,24 @@ match lang:
     case _:
         print("Language Not matched!")
 
+# Loop
+count = 0;
+while count < 5:
+    print("Hello");
+    count += 1;
+
+# for x in range(10):
+#     print(x)
+
+# for x in range(1, 10):
+#     print(x)
+
+# for x in range(1, 10, 2):
+#     print(x)  
+
+for x in range(2, 20, 2):
+    print(x)
+
+Months = ["Jan", "Feb", "Mar", "Apr", "May", "June"];
+for m in Months:
+    print(m);
