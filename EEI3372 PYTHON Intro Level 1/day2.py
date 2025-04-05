65e73418a2564a8f71343465722ea4a493350588
saadrushdi246 <saadrushdi246@gmail.com>
2025-04-05 10:51:14 +0000

    Nested if

---
+++ EEI3372 PYTHON Intro Level 1/day2.py
@@ -0,0 +1,13 @@
+a = 32;
+b = 50;
+c = 15;
+
+if a>b:
+    if a>c:
+        print("a value is big")
+    else:
+        print("c value is big")
+elif b>c:
+    print("b value is big")
+else:
+    print("c is big")
