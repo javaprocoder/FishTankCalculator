94c670495fa9a45d6fc1de6be9ccd04a9ef40e82
saadrushdi246 <saadrushdi246@gmail.com>
2025-04-05 11:05:43 +0000

    Switch statement.

--- EEI3372 PYTHON Intro Level 1/day2.py
+++ EEI3372 PYTHON Intro Level 1/day2.py
@@ -1,1 +1,12 @@
 # SWITCH
+lang = "Python";
+match lang:
+    case "Javascript":
+        print("Web Developer!")
+    case "Java":
+        print("Mobile and Backend Developer!")
+    case "Python":
+        print("Data Scientist!")
+    case _:
+        print("Language Not matched!")
+
