e1435f867a7ea93aad0386b694733c0f5a54e0cb
saadrushdi246 <saadrushdi246@gmail.com>
2025-05-10 13:20:26 +0000

    day4 intro to classes

---
+++ EEI3372 PYTHON Intro Level 1/day4.py
@@ -0,0 +1,13 @@
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+class Student(Person):
+    pass
