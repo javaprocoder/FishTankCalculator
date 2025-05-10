e16dc381006b4a8eec036e8a6223479ddadb4c0d
saadrushdi246 <saadrushdi246@gmail.com>
2025-05-10 14:00:58 +0000

    Polymorphism in Python

--- EEI3372 PYTHON Intro Level 1/day4.py
+++ EEI3372 PYTHON Intro Level 1/day4.py
@@ -1,13 +1,55 @@
-class Dog:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Dog:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
 
-class Car:
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
 
-class Student(Person):
-    pass
+# class Student(Person):
+#     pass
+
+
+#--------------Print A--------------
+# class Base:
+#     def __init__(self):
+#         self._a = "a"
+#         self._c = "ccc"
+
+#     def access_c(self):
+#         return self._c
+
+#     def access_a(self):
+#         return self._a
+
+# x = Base()
+# print(x.access_a())
+
+#--------------Classes and Usage--------------
+class Animal:
+    def speak(self):
+        print("Some Sound!")
+
+class Dog(Animal):
+    def speak(self):
+        print("Bow Bow!")
+
+class Cat(Animal):
+    def speak(self):
+        print("Meow Meow!")
+
+a = Animal()
+a.speak
+
+b = Dog()
+b.speak
+
+#--------------Polymorphism Principle--------------
+def add(num1, num2, num3=0):
+    return num1+num2+num3
+
+print(add(1,2))
+print(add(1,2,3))
